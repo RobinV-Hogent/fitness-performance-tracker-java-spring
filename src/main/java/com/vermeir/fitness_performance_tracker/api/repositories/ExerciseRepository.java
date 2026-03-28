9ff25b9fdc11bc4d84671c652571889b1b65dcf7
Robin Vermeir <robin.v.vermeir@gmail.com>
2026-03-28 16:26:49 +0100

    repo testing exercises

--- src/main/java/com/vermeir/fitness_performance_tracker/api/repositories/ExerciseRepository.java
+++ src/main/java/com/vermeir/fitness_performance_tracker/api/repositories/ExerciseRepository.java
@@ -5,11 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.vermeir.fitness_performance_tracker.api.models.Exercise;
 
 public interface ExerciseRepository extends JpaRepository<Exercise, Integer> {
-
-	@Override
-	default Exercise getById(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
-	}
 	
 }
